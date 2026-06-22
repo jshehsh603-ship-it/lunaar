f44827a76373fe2245f4484363787237f8c62b71
Psquare <psquare@Psquares-MacBook-Air.local>
2026-06-23 00:59:47 +0545

    Disable static export on Vercel builds

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
-  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  output: (process.env.NODE_ENV === 'production' && !process.env.VERCEL) ? 'export' : undefined,
   images: {
     unoptimized: true,
   },
