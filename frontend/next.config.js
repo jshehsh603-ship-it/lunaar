01d799435b7e3661c06308b1a1acecb059adf4d3
Psquare <psquare@Psquares-MacBook-Air.local>
2026-06-23 01:04:17 +0545

    Remove distDir from Next.js config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NODE_ENV === 'development' ? '.next-dev' : '.next',
   output: (process.env.NODE_ENV === 'production' && !process.env.VERCEL) ? 'export' : undefined,
   images: {
     unoptimized: true,
